9c2b41164a245a71fe26ce8856a69827e95756ec
laigaaming <laigaaming@163.com>
2018-03-18 00:28:04 +0800

    Supplement config.py

---
+++ src/main/fx/config.py
@@ -0,0 +1,23 @@
+#-------------------------------------------------------------------------------
+# Name:        config.py
+# Purpose:
+#
+# Author:      laigaaming
+#
+# Created:
+# Copyright:   (c) www.laigaaming.com
+# Licence:
+#-------------------------------------------------------------------------------
+
+global appkey
+global sign
+global format
+global database
+global db_table
+
+appkey = "<Fill in your API key set here>"
+sign = "<Fill in your API signature here>"
+format = "json"
+
+database = '<Fill in your database instance name here>'
+db_table = '<Fill in your database table here>'
